fc5d6f7abc04acbfa1982e5e31b84a127835e771
savaged-22 <juanpabloespinosa2014@gmail.com>
2024-11-05 10:27:39 -0500

    Crear modelo de producto e implementar api para crear productos. Closes #7

--- src/main/java/org/fis/inventorymanagement/services/ProductService.java
+++ src/main/java/org/fis/inventorymanagement/services/ProductService.java
@@ -1,6 +1,7 @@
 package org.fis.inventorymanagement.services;
 
 import org.fis.inventorymanagement.models.Product;
+import org.fis.inventorymanagement.models.product;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -18,4 +19,37 @@ public class ProductService {
     );
 
     // TODO: Implement the methods for the ProductService class
+
+
+    public Product createProduct(Product product){
+        products.add(product);
+        return product;
+    }
+    public List<Product> getAllProducts() {
+        return products;
+    }
+
+    public Product getProductById(int id) {
+        return products.stream()
+                .filter(product -> product.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Product updateproduct(int id, Product updatedproduct) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                product.setId(updatedproduct.getId());
+                product.setName(updatedproduct.getName());
+                product.setPrice(updatedproduct.getPrice());
+                product.setStock(updatedproduct.getStock());
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public void deleteproduct(int id) {
+        products.removeIf(product -> product.getId() == id);
+    }
 }
